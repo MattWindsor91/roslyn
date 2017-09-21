6b89416ac65abc4292740296bda0ef8854534864
Matthew Windsor <t-mawi@microsoft.com>
2017-09-21 15:51:38 +0100

    ConceptExtensions: Stub out testbed
    This has concepts with extension methods, but no syntax sugar.
    
    Does not yet compile.

---
+++ concepts/code/ConceptExtensionMethods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Concepts;
+using System.Linq;
+
+namespace ConceptExtensionMethods
+{
+    public concept CMonoid<T>
+    {
+        [ConceptExtension]
+        T Plus(T me, T you);
+
+        T Zero { get; }
+    }
+
+    public instance Monoid_List<T> : CMonoid<List<T>>
+    {
+        [ConceptExtension]
+        List<T> Plus(List<T> me, List<T> you) => new List<T>(me.Concat(you));
+
+        List<T> Zero => new List<T>();
+    }
+        
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Static invocation
+            System.Console.Write("Static:");
+            foreach (var i in CMonoid<List<int>>.Plus(new List<int> { 1, 2, 3 }, new List<int> { 4, 5, 6 }))
+            {
+                System.Console.Write(" ");
+                System.Console.Write(i);
+            }
+            System.Console.WriteLine();
+
+            // Extension invocation
+            System.Console.Write("CEM:   ");
+            foreach (var i in (new List<int> { 1, 2, 3 }).Plus(new List<int> { 4, 5, 6 }))
+            {
+                System.Console.Write(" ");
+                System.Console.Write(i);
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
