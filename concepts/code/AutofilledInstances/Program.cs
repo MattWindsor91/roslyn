969143498f03a87d50684ed911de21457d0408f8
Matthew Windsor <t-mawi@microsoft.com>
2017-10-12 17:25:36 +0100

    Examples: add to/improve feature examples
    These still need a bit more work before the LDM.

--- concepts/code/AutofilledInstances/Program.cs
+++ concepts/code/AutofilledInstances/Program.cs
@@ -2,28 +2,41 @@
 
 namespace AutofilledInstances
 {
+    // # Autofilled Instances
+    //
+    // Sometimes, concept instances are thin wrappers over existing
+    // functionality in a type.  Writing instances entirely consisting of
+    // `TResult Method(this T1 x, T2 y) => x.Method(y)` would be tedious,
+    // so Concept-C# has some support for 'autofilling' such methods.
+
+    // Suppose we want to abstract over types that are monoids:
+
     public concept CMonoid<T>
     {
         T Plus(this T me, T you);
-
         T Zero { get; }
     }
 
+    // Suppose we also have a class that has a `Plus` method that not only
+    // lines up semantically with monoidal append, but in fact has the same
+    // signature:
+
     struct Pair
     {
-        public int x;
-        public int y;
-
-        public Pair Plus(Pair you)
-        {
-            return new Pair { x = x + you.x, y = y + you.y };
-        }
+        public int x, y;
+        public Pair Plus(Pair you) => new Pair { x = x + you.x, y = y + you.y };
     }
+
+    // If we open an instance for CMonoid<Pair>, Concept-C# can automatically
+    // detect that CMonoid<Pair>.Plus can be implemented using Pair.Plus, and
+    // will create the stub call for us:
 
     instance Monoid_Pair : CMonoid<Pair>
     {
         Pair Zero => new Pair { x = 0, y = 0 };
     }
+
+    // We can test this as follows:
 
     class Program
     {
@@ -31,7 +44,6 @@ namespace AutofilledInstances
         {
             var p1 = new Pair { x = 30, y = 31 };
             var p2 = new Pair { x = 95, y = 98 };
-
             Console.WriteLine($"{p1.Plus(p2).x}, {p1.Plus(p2).y}");
         }
     }
---
+++ concepts/code/StandaloneInstances/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace StandaloneInstances
+{
+    class Program
+    {
+        // # Standalone instances
+        //
+        // If an instance does not implement any concepts, Concept-C# lets its
+        // concept extension methods and operator overloads be accessed as if
+        // they were attached to a concept in scope.
+        //
+        // This makes instances become a more powerful form of extension class.
+
+        public instance DoubleInt
+        {
+            int Double(this int x) => x * x;
+        }
+
+        // Like normal extension methods, .Double() is now available to ints:
+
+        public static void TestDoubleInt()
+        {
+            Console.WriteLine(5.Double());  // 10
+        }
+
+        // Standalone instances can also define operator overloads.
+        // Defining one on 'int' to break the normal int behaviour looks
+        // possible...
+
+        public instance BreakInt
+        {
+            int operator +(int x, int y) => 5;
+        }
+
+        // ...but since concept operator overloads are prioritised below
+        // other types of operator overload, this won't actually happen:
+
+        public static void TestBreakInt()
+        {
+            Console.WriteLine(2 + 2);  // 4
+        }
+
+        static void Main(string[] args)
+        {
+            TestDoubleInt();
+            TestBreakInt();
+        }
+    }
+}
