465052e84c90ea969dcdfeaf5d89f2220e017864
Matthew Windsor <t-mawi@microsoft.com>
2017-10-15 15:19:13 +0100

    Concept-C#'17: check all shim validity by binding calls
    Instead of doing a really horrible signature comparison pass to
    decide whether shims are available, just bind the call and check
    for errors.
    
    Might be slower, but should be more complete.

--- src/Compilers/CSharp/Portable/Symbols/Source/SourceMemberContainerSymbol_Concept.cs
+++ src/Compilers/CSharp/Portable/Symbols/Source/SourceMemberContainerSymbol_Concept.cs
@@ -144,33 +144,22 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             Debug.Assert(concept.IsConcept, "concept for default implementation synthesis must be an actual concept");
             Debug.Assert(IsInstance, "target for default implementation synthesis must be an instance");
 
-            if (conceptMethod.IsOperator())
+            var ometh = TrySynthesizeOperatorShim(conceptMethod);
+            if (ometh != null && ometh.IsValid())
             {
-                var ometh = TrySynthesizeOperatorShim(concept, conceptMethod, diagnostics);
-                if (ometh != null)
-                {
-                    return ometh;
-                }
+                return ometh;
             }
-
-            if (conceptMethod.IsConceptExtensionMethod)
+            var emeth = TrySynthesizeConceptExtensionShim(conceptMethod);
+            if (emeth != null && emeth.IsValid())
             {
-                var emeth = TrySynthesizeConceptExtensionShim(concept, conceptMethod, diagnostics);
-                if (emeth != null)
-                {
-                    return emeth;
-                }
+                return emeth;
             }
 
             // Intentionally synthesize defaults as a last resort.
-            var dstr = concept.GetDefaultStruct();
-            if (dstr != null)
+            var dmeth = TrySynthesizeDefaultShim(concept, conceptMethod, diagnostics);
+            if (dmeth != null && dmeth.IsValid())
             {
-                var dmeth = TrySynthesizeDefaultShim(dstr, concept, conceptMethod, diagnostics);
-                if (dmeth != null)
-                {
-                    return dmeth;
-                }
+                return dmeth;
             }
 
             return null;
@@ -180,144 +169,52 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
         /// Tries to generate an instance shim mapping a concept operator
         /// to a static operator on the operator's first parameter type.
         /// </summary>
-        /// <param name="concept">
-        /// The concept containing the default struct.
-        /// </param>
         /// <param name="conceptMethod">
         /// The concept method we're implementing on an instance with a shim.
         /// </param>
-        /// <param name="diagnostics">
-        /// A diagnostics set, to which we report default struct failures.
-        /// </param>
         /// <returns>
         /// Null if we couldn't synthesize a shim; otherwise, the created shim.
+        /// The shim might not be valid: this must be checked before acceptance.
         /// </returns>
-        private SynthesizedOperatorShimMethod TrySynthesizeOperatorShim(NamedTypeSymbol concept, MethodSymbol conceptMethod, DiagnosticBag diagnostics)
+        private SynthesizedOperatorShimMethod TrySynthesizeOperatorShim(MethodSymbol conceptMethod)
         {
             // TODO(MattWindsor91): consider opening this up, with
             //     restrictions, to any static method.
-            Debug.Assert(conceptMethod.IsOperator(),
-                "shouldn't synthesize an operator shim on a non-operator");
+            if (!conceptMethod.IsOperator())
+            {
+                return null;
+            }
             Debug.Assert(0 < conceptMethod.ParameterCount,
                 "concept operator should have at least one parameter");
-
-            var target = conceptMethod.ParameterTypes[0];
-            var shim = new SynthesizedOperatorShimMethod(conceptMethod, this);
-            return shim.IsValid() ? shim : null;
+            return new SynthesizedOperatorShimMethod(conceptMethod, this);
         }
-        
+
         /// <summary>
         /// Tries to generate an instance shim mapping a concept extension method
         /// to an actual method on its 'this' parameter's type.
         /// </summary>
-        /// <param name="concept">
-        /// The concept containing the default struct.
-        /// </param>
         /// <param name="conceptMethod">
         /// The concept method we're implementing on an instance with a shim.
         /// </param>
-        /// <param name="diagnostics">
-        /// A diagnostics set, to which we report default struct failures.
-        /// </param>
         /// <returns>
         /// Null if we couldn't synthesize a shim; otherwise, the created shim.
+        /// The shim might not be valid: this must be checked before acceptance.
         /// </returns>
-        private SynthesizedConceptExtensionShimMethod TrySynthesizeConceptExtensionShim(NamedTypeSymbol concept, MethodSymbol conceptMethod, DiagnosticBag diagnostics)
+        private SynthesizedConceptExtensionShimMethod TrySynthesizeConceptExtensionShim(MethodSymbol conceptMethod)
         {
-            Debug.Assert(conceptMethod.IsConceptExtensionMethod,
-                "shouldn't try synthesising this on a static concept method");
+            if (!conceptMethod.IsConceptExtensionMethod)
+            {
+                return null;
+            }
             Debug.Assert(0 < conceptMethod.ParameterCount,
                 "concept extension method should have a 'this' parameter");
-
-            var target = conceptMethod.ParameterTypes[0];
-            ImmutableArray<Symbol> members;
-            if (target.Kind == SymbolKind.NamedType)
-            {
-                var nt = (NamedTypeSymbol)target;
-                members = nt.GetSimpleNonTypeMembers(conceptMethod.Name);
-            }
-            else
-            {
-                members = target.GetMembers(conceptMethod.Name);
-            }
-
-            foreach (var member in members)
-            {
-                if (member.Kind == SymbolKind.Method
-                    && CanMakeConceptExtensionShim(from: conceptMethod, to: (MethodSymbol)member))
-                {
-                    return new SynthesizedConceptExtensionShimMethod(conceptMethod, this);
-                }
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        /// Can we make a concept extension shim between these methods?
-        /// </summary>
-        /// <param name="from">
-        /// The concept method we're trying to implement with a shim.
-        /// </param>
-        /// <param name="to">
-        /// The method we're trying to call into with a shim.
-        /// </param>
-        /// <returns>
-        /// Whether it would be valid for a concept extension shim to implement
-        /// <paramref name="from"/> by calling into
-        /// <paramref name="to"/>.
-        /// </returns>
-        private static bool CanMakeConceptExtensionShim(MethodSymbol from, MethodSymbol to)
-        {
-            // TODO(@MattWindsor91): these checks are flaky, and likely
-            //     incomplete.
-            if (to.IsStatic)
-            {
-                return false;
-            }
-            if (from.Arity != to.Arity)
-            {
-                return false;
-            }
-            // TODO(@MattWindsor91): is this too strict?
-            if (to.DeclaredAccessibility != Accessibility.Public)
-            {
-                return false;
-            }
-            // Remember: `to` has an implicit 'this', `from` does not.
-            if (from.ParameterCount - 1 != to.ParameterCount)
-            {
-                return false;
-            }
-
-            // To compare, we must rewrite `from` from
-            // B(A, C, D) to A.B(C, D).
-            var extParamsB = ArrayBuilder<ParameterSymbol>.GetInstance();
-            extParamsB.AddRange(from.Parameters.Skip(1));
-            var extParams = extParamsB.ToImmutableAndFree();
-            var shuffledFrom = new SignatureOnlyMethodSymbol(
-                from.Name,
-                from.ContainingType,
-                from.MethodKind,
-                from.CallingConvention,
-                from.TypeParameters,
-                extParams,
-                from.RefKind,
-                from.ReturnType,
-                from.ReturnTypeCustomModifiers,
-                from.RefCustomModifiers,
-                from.ExplicitInterfaceImplementations);
-            return MemberSignatureComparer.CSharpImplicitImplementationComparer.Equals(
-                shuffledFrom, to);
+            return new SynthesizedConceptExtensionShimMethod(conceptMethod, this);
         }
 
         /// <summary>
         /// Tries to generate an instance shim mapping a concept method to
         /// one on the given default struct.
         /// </summary>
-        /// <param name="dstr">
-        /// The default struct into which the shim will call.
-        /// </param>
         /// <param name="concept">
         /// The concept containing the default struct.
         /// </param>
@@ -329,9 +226,16 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
         /// </param>
         /// <returns>
         /// Null if we couldn't synthesize a shim; otherwise, the created shim.
+        /// The shim might not be valid: this must be checked before acceptance.
         /// </returns>
-        private SynthesizedDefaultShimMethod TrySynthesizeDefaultShim(NamedTypeSymbol dstr, NamedTypeSymbol concept, MethodSymbol conceptMethod, DiagnosticBag diagnostics)
+        private SynthesizedDefaultShimMethod TrySynthesizeDefaultShim(NamedTypeSymbol concept, MethodSymbol conceptMethod, DiagnosticBag diagnostics)
         {
+            var dstr = concept.GetDefaultStruct();
+            if (dstr == null)
+            {
+                return null;
+            }
+
             // Default-struct sanity checking
             var conceptLoc = concept.Locations.IsEmpty ? Location.None : Locations[0];
             var instanceLoc = Locations.IsEmpty ? Location.None : Locations[0];
@@ -349,24 +253,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                 return null;
             }
 
-            // Check that the defaults struct actually contains this method
-            // TODO(MattWindsor91): check this works for properties
-            var eco = MemberSignatureComparer.CSharpImplicitImplementationComparer;
-            foreach (var member in dstr.GetMembersUnordered())
-            {
-                if (member.Kind != SymbolKind.Method)
-                {
-                    continue;
-                }
-                if (!eco.Equals(conceptMethod, member))
-                {
-                    continue;
-                }
-
-                return new SynthesizedDefaultShimMethod(conceptMethod, dstr, this);
-            }
-
-            return null;
+            return new SynthesizedDefaultShimMethod(conceptMethod, dstr, this);
         }
 
         #endregion Shim synthesis
